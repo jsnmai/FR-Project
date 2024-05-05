8db44c4b2242d43c662e90ff8ade303b1bba68a3
May <mcb12@williams.edu>
2024-05-05 15:58:02 -0400

    changes

--- cnn.py
+++ cnn.py
@@ -13,7 +13,10 @@ import matplotlib.pyplot as plt
 from keras.preprocessing import image
 import numpy as np
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
+
+#################################################################
+def data_gen():
+    train_datagen = ImageDataGenerator(rescale = 1./255,
       rotation_range=25,
       width_shift_range=0.2,
       height_shift_range=0.2,
@@ -24,115 +27,170 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 #################################################################
 
-batch_size = 64
-target_size = (64, 64)
-input_shape=(64, 64, 3)
-seed=1337
-adam = 0.001
-fre= -20
-FC = 2048
-E = 1
-patience = 3
-verbose = 1
-factor = 0.50
-min_lr = 0.0001
-steps_per_epoch=256
-validation_steps=256
-epochs=8
+    batch_size = 64
+    target_size = (64, 64)
+    input_shape=(64, 64, 3)
+    seed=1337
+    adam = 0.001
+    fre= -20
+    FC = 2048
+    E = 1
+    patience = 3
+    verbose = 1
+    factor = 0.50
+    min_lr = 0.0001
+    steps_per_epoch=256
+    validation_steps=256
+    epochs=8
+
+    test_datagen = ImageDataGenerator( rescale = 1.0/255)
+
+    train_generator = train_datagen.flow_from_directory('../Dataset/Train',
+                                                        batch_size =batch_size ,
+                                                        class_mode = 'binary',
+                                                        seed=seed,
+                                                        target_size = target_size )     
+
+    validation_generator =  test_datagen.flow_from_directory('../Dataset/Validation',
+                                                            batch_size  = batch_size,
+                                                            class_mode  = 'binary',
+                                                            seed=seed,
+                                                            target_size = target_size)
+
+    #################################################################
+
+    base_model = tf.keras.applications.VGG16(input_shape=input_shape,include_top=False,weights="imagenet")
+
+    #################################################################
+
+    for layer in base_model.layers[:fre]:
+        layer.trainable=False
 
 #################################################################
 
-test_datagen = ImageDataGenerator( rescale = 1.0/255)
+    model=Sequential()
+    model.add(base_model)
+    model.add(layers.Dropout(.2))
 
-train_generator = train_datagen.flow_from_directory('Dataset/Train',
-                                                    batch_size =batch_size ,
-                                                    class_mode = 'binary',
-                                                    seed=seed,
-                                                    target_size = target_size )     
-
-validation_generator =  test_datagen.flow_from_directory( 'Dataset/Validation',
-                                                          batch_size  = batch_size,
-                                                          class_mode  = 'binary',
-                                                          seed=seed,
-                                                          target_size = target_size)
-
-#################################################################
-
-base_model = tf.keras.applications.VGG16(input_shape=input_shape,include_top=False,weights="imagenet")
-
-#################################################################
-
-for layer in base_model.layers[:fre]:
-    layer.trainable=False
-
-#################################################################
-
-# Building Model
-model=Sequential()
-model.add(base_model)
-model.add(layers.Dropout(.2))
-
-model.add(Conv2D(512, (3, 3),strides=(1,1), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(layers.Dropout(.1))
-model.add(Conv2D(128, (3, 3),strides=(1,1), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(layers.Dropout(.1))
-model.add(Conv2D(384, (3, 3),strides=(1,1), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(layers.Dropout(.1))
-model.add(Conv2D(384, (3, 3),strides=(1,1), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(layers.Dropout(.1))
-model.add(Conv2D(500, (3, 3),strides=(1,1), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(2,strides=(2,2), padding='same'))
+    model.add(Conv2D(512, (3, 3),strides=(1,1), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(layers.Dropout(.1))
+    model.add(Conv2D(128, (3, 3),strides=(1,1), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(layers.Dropout(.1))
+    model.add(Conv2D(384, (3, 3),strides=(1,1), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(layers.Dropout(.1))
+    model.add(Conv2D(384, (3, 3),strides=(1,1), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(layers.Dropout(.1))
+    model.add(Conv2D(500, (3, 3),strides=(1,1), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(2,strides=(2,2), padding='same'))
 
 
 
-# Add new layers
-model.add(Flatten())
-model.add(Dense(FC , activation='relu'))
-model.add(layers.Dropout(.2))
-model.add(Dense(FC , activation='relu'))
-model.add(layers.Dropout(.2))
-model.add(Dense(FC, activation='relu'))
-model.add(layers.Dropout(.2))
-model.add(Dense(E, activation='sigmoid'))
+    # Add new layers
+    model.add(Flatten())
+    model.add(Dense(FC , activation='relu'))
+    model.add(layers.Dropout(.2))
+    model.add(Dense(FC , activation='relu'))
+    model.add(layers.Dropout(.2))
+    model.add(Dense(FC, activation='relu'))
+    model.add(layers.Dropout(.2))
+    model.add(Dense(E, activation='sigmoid'))
 
-model.summary()
+    model.summary()
 
 #################################################################
 
-model.compile(optimizer=Adam(adam),
-              loss='binary_crossentropy'
-              ,metrics=['accuracy'])
+    model.compile(optimizer=Adam(adam),
+                loss='binary_crossentropy'
+                ,metrics=['accuracy'])
 
 #################################################################
 
-lrd = ReduceLROnPlateau(monitor = 'val_loss',
-                        patience = patience,
-                        verbose = verbose ,
-                        factor = factor,
-                        min_lr = min_lr)
+    lrd = ReduceLROnPlateau(monitor = 'val_loss',
+                            patience = patience,
+                            verbose = verbose ,
+                            factor = factor,
+                            min_lr = min_lr)
 
 
 
-es = EarlyStopping(verbose=verbose, patience=patience)
+    es = EarlyStopping(verbose=verbose, patience=patience)
+    return model
 
 #################################################################
 
-#  images test 1
-path_testmodel = "842.jpg"
-imge = image.load_img(path_testmodel, target_size=target_size)
-X = image.img_to_array(imge)
-X = np.expand_dims(X, axis=0)
 
-images = np.vstack([X])
-classes = model.predict(images, batch_size=1)
-print(classes[0])
-if classes[0]<0.5:
-    print("This is a male")
-else:
-    print( "This  is a female")
-plt.imshow(imge)
+def classify(model, imagepath):
+    imge = image.load_img(imagepath, target_size=(64, 64))
+    X = image.img_to_array(imge)
+    X = np.expand_dims(X, axis=0)
+
+    images = np.vstack([X])
+    classes = model.predict(images, batch_size=8)
+    print(classes[0])
+    if classes[0]<0.5:
+        return "male"
+    else:
+        return "female"
+    # plt.imshow(imge)
+
+def plot_average(datasets, m_avg, f_avg):
+    genders = {
+        'CASIA-Webface': (m_avg, f_avg),
+        'SFace-60': (m_avg, f_avg)
+    }
+
+    x = np.arange(len(datasets))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in genders.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=2)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    fig.suptitle('Dataset Gender Distribution', fontsize=20)
+    ax.set_ylabel('Percentage Identified')
+    ax.set_title('Gender Attributes')
+    ax.set_xticks(x + width, genders)
+    ax.legend(loc='upper left', ncols=2)
+    ax.set_ylim(0, 100)
+
+    plt.show()
+
+if __name__ == "__main__" :
+    males = 0
+    females = 0
+    model = data_gen()
+    datasets = ("Baseline", "SFace-60")
+    i = 0
+    for subfolder in os.listdir("../Dataset/Test/"):
+        subname = os.path.join("../Dataset/Test/", subfolder)
+        for filename in os.listdir(subname):
+            f = os.path.join(subname, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                if i < 50:
+                    print(f)
+                    gender = classify(model, f)
+                    if gender == "male":
+                        males += 1
+                        i += 1
+                    elif gender == "female":
+                        females += 1
+                        i += 1
+                    pass
+                elif i == 51:
+                    print("done")
+            else:
+                print("not a good path to images")
+                pass
+    plot_average(datasets, males, females)
